80b6d86e4aa7375d3803aec150414aa065b62ee2
Kyohei-us <kyohei1212000@gmail.com>
2022-07-15 11:10:07 +0000

    Add: methods for Post/Tag

--- src/post/index.ts
+++ src/post/index.ts
@@ -1,18 +1,26 @@
-import { Post, PrismaClient } from "@prisma/client";
+import {Post, PrismaClient, Tag} from '@prisma/client';
 
 const prisma = new PrismaClient();
 /**
  * @return {Promise}
  */
 export async function getAllPosts(): Promise<Post[]> {
-  const all = await prisma.post.findMany();
+  const all = await prisma.post.findMany({
+    include: {
+      postInfo: {
+        include: {
+          tags: true
+        }
+      }
+    },
+  });
   return all;
 }
 /**
  * @param  {{title:string, body:string}} args
  * @return {Promise}
  */
- export async function createPost(
+export async function createPost(
     args: {title:string, body:string},
 ): Promise<Post> {
   const {title, body} = args;
@@ -22,6 +30,111 @@ export async function getAllPosts(): Promise<Post[]> {
       body: body,
       createdAt: new Date(),
     },
+    include: {
+      postInfo: true,
+    },
   });
   return post;
+}
+
+/**
+ * @param  {{id:number, title: string?, body: string?}} args
+ * @return {Promise}
+ */
+export async function updatePost(
+    args: {
+    id: number,
+    title?: string,
+    body?: string
+  },
+): Promise<Post> {
+  // Use destruction to extract
+  // id field and the rest.
+  // Note: updateData does not have id field.
+  const {id, ...updateData} = args;
+  // Question: if update data for
+  // title field is not given,
+  // what will happen?
+  // -> updateData won't have title field,
+  // so title field won't get updated in any way.
+  console.log('updateData:', updateData);
+  const updated = await prisma.post.update({
+    where: {
+      id: id,
+    },
+    data: {
+      ...updateData,
+    },
+    include: {
+      postInfo: true,
+    },
+  });
+  return updated;
+}
+
+export async function getAllTags(): Promise<Tag[]> {
+  const allTags = await prisma.tag.findMany({
+    include: {
+      postInfos: {
+        include: {
+          tags: true
+        }
+      }
+    }
+  });
+  return allTags;
+}
+
+export async function addTags(
+  args: {
+    postId: number,
+    tags: string[]
+  }
+): Promise<Post> {
+  const {postId, tags} = args;
+  tags.forEach(async (tag)=>{
+    await prisma.tag.upsert({
+      where: {
+        name: tag
+      },
+      update: {},
+      create: {
+        name: tag
+      },
+    });
+  })
+  const upserted = await prisma.post.update({
+    where: {
+      id: postId,
+    },
+    data: {
+      updatedAt: new Date(),
+      postInfo: {
+        upsert: {
+          create: {
+            tags: {
+              connect: tags.map((tag) => {
+                return {"name": tag}
+              })
+            }  
+          },
+          update: {
+            tags: {
+              connect: tags.map((tag) => {
+                return {"name": tag}
+              })
+            } 
+          }
+        }
+      }
+    },
+    include: {
+      postInfo: {
+        include: {
+          tags: true
+        }
+      }
+    }
+  });
+  return upserted;
 }
